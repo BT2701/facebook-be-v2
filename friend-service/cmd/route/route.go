b834e79cb87eba26c0cef3cebf04613109fc9dd0
Dương Thành Trưởng <dttruonga8tqtpy@gmail.com>
2025-01-10 11:38:45 +0700

    routing for friend-service

---
+++ friend-service/cmd/route/route.go
@@ -0,0 +1,50 @@
+package route
+
+import (
+	"os"
+	"friend-service/internal/adapters/inbound"
+	"friend-service/internal/adapters/outbound"
+	"friend-service/internal/app/service"
+	"friend-service/pkg/database"
+	"friend-service/pkg/utils"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func SetupRouter() *echo.Echo {
+	// Initialize MongoDB connection
+	database.InitMongoDB()
+	databaseName := os.Getenv("DB_NAME")
+	friendCollection := database.GetCollection(databaseName, "friends")
+
+	// Create repositories and services
+	friendRepo := outbound.NewFriendRepository(friendCollection)
+	friendService := service.NewFriendService(friendRepo)
+
+
+	// Create handlers
+	friendHandler := inbound.NewFriendHandler(friendService)
+
+	// Set up Echo
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+			return next(c)
+		}
+	})
+	e.Use(utils.CorsMiddleware())
+	e.POST("/friends", friendHandler.CreateFriend)
+	e.GET("/friends/:id", friendHandler.GetFriend)
+	e.PUT("/friends/:id", friendHandler.UpdateFriend)
+	e.DELETE("/friends/:id", friendHandler.DeleteFriend)
+	e.GET("/friends", friendHandler.GetFriends)
+	e.GET("/friends/:userID/friends", friendHandler.GetFriendsByUserID)
+	e.GET("/friends/:userID1/:userID2", friendHandler.IsFriend)
+	return e
+}
